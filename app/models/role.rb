2bd8e2404c3ba8ffc899c0de976c54388dda6bc3
vaidhesh <vaidheessct@gmail.com>
2015-12-23 12:41:01 +0530

    hai

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,13 @@
 class Role < ActiveRecord::Base
+  belongs_to :users
+  before_create :set_priority
+
+
+
+  private
+  def set_priority
+    if @current_user.has_role? :admin
+      self.priority = 1
+    end
+  end
 end
