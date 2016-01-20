0db43ede1bd31528738eec58d5b4b8dd9a4ee97e
vaidhesh <vaidheessct@gmail.com>
2016-01-20 15:55:51 +0530

    msg

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,58 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151222070615) do
+ActiveRecord::Schema.define(version: 20160105074100) do
+
+  create_table "clients", force: :cascade do |t|
+    t.string   "name"
+    t.string   "site"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "clients_documents", id: false, force: :cascade do |t|
+    t.integer "client_id",   null: false
+    t.integer "document_id", null: false
+  end
+
+  add_index "clients_documents", ["client_id", "document_id"], name: "index_clients_documents_on_client_id_and_document_id"
+  add_index "clients_documents", ["document_id", "client_id"], name: "index_clients_documents_on_document_id_and_client_id"
+
+  create_table "clients_teams", id: false, force: :cascade do |t|
+    t.integer "client_id", null: false
+    t.integer "team_id",   null: false
+  end
+
+  add_index "clients_teams", ["client_id", "team_id"], name: "index_clients_teams_on_client_id_and_team_id"
+  add_index "clients_teams", ["team_id", "client_id"], name: "index_clients_teams_on_team_id_and_client_id"
+
+  create_table "documents", force: :cascade do |t|
+    t.string   "title"
+    t.string   "author"
+    t.string   "team_name"
+    t.string   "summary"
+    t.string   "ref_link"
+    t.string   "file_path"
+    t.integer  "client_ids"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "content_type"
+    t.string   "fileimg_path"
+    t.string   "file_name"
+    t.string   "file_image"
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,7 +72,8 @@ ActiveRecord::Schema.define(version: 20151222070615) do
     t.string   "password_confirmation"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
-    t.integer  "role"
+    t.boolean  "admin"
+    t.integer  "role_id"
   end
 
 end
